35e578efe499845b792ebb6cc0db52abd2a84907
djx <jxce_d@163.com>
2017-09-15 19:06:30 +0800

    ok

---
+++ Dj.IRepository/IBaseRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJ.IRepository
+{
+    /// <summary>
+    /// 数据层父接口
+    /// </summary>
+    /// <typeparam name="TEntity">实体类，对应要操作的表</typeparam>
+    public interface IBaseRepository<TEntity> where TEntity:class
+    {
+        int Add(TEntity entity);
+        int Remove(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ Dj.Repository/BaseRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DJ.IRepository;
+using DJ.Models;
+using System.Data.Entity;
+
+namespace DJ.Repository
+{
+    public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
+    {
+        DbContext db = new RegSysEntities();
+        DbSet<TEntity> _dbset;
+        public BaseRepository()
+        {
+            _dbset = db.Set<TEntity>();
+        }
+        public int Add(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Remove(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
