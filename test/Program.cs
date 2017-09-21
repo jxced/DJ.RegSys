a7007b26d930c48831a4755251f8e2042a13acf2
djx <jxce_d@163.com>
2017-09-21 13:18:25 +0800

    ok

--- test/Program.cs
+++ test/Program.cs
@@ -10,7 +10,7 @@ namespace test
             pig.Add();
            
             Pig p= pig as Pig;
-            p.
+            
             Console.ReadKey();
             Pig pigs = new Pig();
             pigs.Add();
