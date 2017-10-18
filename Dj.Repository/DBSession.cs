bb363f4914be4d9d10f30efdc7cdd9ff2dc8a4a7
djx <jxce_d@163.com>
2017-10-18 17:48:58 +0800

    ok

---
+++ Dj.Repository/DBSession.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJ.IRepository
+{
+    /// <summary>
+    /// 数据仓储接口，作用：
+    /// 1.调用EF容器 批量 执行 sql语句
+    /// 2.方便通过 子接口属性直接 获取 对应数据表的操作接口对象
+    /// </summary>
+    public partial interface IDBSession
+    {
+        int SaveChanges();
+    }
+}
