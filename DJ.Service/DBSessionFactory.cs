cc63fb66af3f9cb2323a2171859f90d81c5e2414
djx <jxce_d@163.com>
2017-10-22 17:49:40 +0800

    ok

---
+++ DJ.Service/DBSessionFactory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DJ.Service
+{
+   public class DBSessionFactory
+    {
+       public static IRepository.IDBSession GetDBSession()
+        {
+            var db= CallContext.GetData("DBSession") as IRepository.IDBSession;
+            if (db==null)
+            {
+                db = DJ.Utility.DI.GetObject<IRepository.IDBSession>("DBSession");
+                CallContext.SetData("DBSession", db);
+            }
+            return db;
+        }
+    }
+}
