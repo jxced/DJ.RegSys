3c494c074147d0a275e1f05ec82d74d14a8d06dd
djx <jxce_d@163.com>
2017-10-30 12:05:17 +0800

    ok

---
+++ DJ.Models/TemplateModel/LoginVEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DJ.Models.ViewModels
+{
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    public class LoginVEntity
+    {
+        [Required(ErrorMessage ="用户名不能为空")]
+        public string Email { get; set; }
+        [Required(ErrorMessage ="密码不能为空")]
+        [MinLength(8,ErrorMessage ="密码长度至少8位")]
+        public string Pwd { get; set; }
+    }
+}
