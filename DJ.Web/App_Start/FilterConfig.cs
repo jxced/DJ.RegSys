5064e9d0568bfc73f0aac580414c3c306f8b98a4
djx <jxce_d@163.com>
2017-11-18 02:23:41 +0800

    ok

--- DJ.Web/App_Start/FilterConfig.cs
+++ DJ.Web/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace DJ.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filter.AuthorizationAttribute());
         }
     }
 }
---
+++ DJ.Web/Filter/AuthorizationAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DJ.Web.Filter
+{
+    public class AuthorizationAttribute:AuthorizeAttribute
+    {
+        public override void OnAuthorization(AuthorizationContext filterContext)
+        {
+            base.OnAuthorization(filterContext);
+        }
+    }
+}
